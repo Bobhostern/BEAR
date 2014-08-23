2c86acbb5c0184dfbad820becc9a5505be174b45
Bob Hosten <bob.hostern@gmail.com>
2014-08-22 20:32:10 -0500

    Tabs get sooooo annoying sometimes...

--- src/bear.rs
+++ src/bear.rs
@@ -17,60 +17,60 @@ mod interpret;
 
 #[deriving(Decodable, Show)]
 struct BearOpts {
-	file: Option<String>,
-	help: bool,
-	interactive: bool
+    file: Option<String>,
+    help: bool,
+    interactive: bool
 }
 
 hammer_config!(BearOpts "BEAR - Another BF",
-	|c| { 
-		c.short("file", 'f').short("help", 'h').short("interactive", 'i')
+    |c| { 
+        c.short("file", 'f').short("help", 'h').short("interactive", 'i')
     }
 )
 
 fn interactive_console() {
-	let mut mem = Memory::new();
+    let mut mem = Memory::new();
 
     print!("> ");
-   	for line in io::stdin().lines() {
-   		match line {
-   			Ok(val) => { interpret(val, &mut mem); print!("\n> "); },
-   			Err(err) => fail!(err.desc)
-   		};
+    for line in io::stdin().lines() {
+        match line {
+            Ok(val) => { interpret(val, &mut mem); print!("\n> "); },
+            Err(err) => fail!(err.desc)
+        };
     }
 }
 
 fn main() {
-	let opts: BearOpts = match decode_args(os::args().tail()) {
-		Ok(val) => val,
-		Err(err) => fail!(err.message)
-	};
-	if opts.help {
-		let (desc, usage_text) = usage::<BearOpts>(true);
-	    println!("Usage: {}", os::args().get(0));
-	    println!("{}", usage_text);
-	    println!("{}", desc.unwrap());
-	}
-	else if opts.interactive {
+    let opts: BearOpts = match decode_args(os::args().tail()) {
+        Ok(val) => val,
+        Err(err) => fail!(err.message)
+    };
+    if opts.help {
+        let (desc, usage_text) = usage::<BearOpts>(true);
+        println!("Usage: {}", os::args().get(0));
+        println!("{}", usage_text);
+        println!("{}", desc.unwrap());
+    }
+    else if opts.interactive {
         interactive_console();
-	}
-	else {
-		let filename = match opts.file {
-			Some(val) => val,
-			None => fail!("--file required")
-		};
-		let test = match File::open(&Path::new(filename)).read_to_end() {
-			Ok(val) => { 
-				match String::from_utf8(val) {
-					Ok(val) => val,
-					Err(err) => fail!(err)
-				} 
-			},
-			Err(err) => fail!(err.desc)
-		};
+    }
+    else {
+        let filename = match opts.file {
+            Some(val) => val,
+            None => fail!("--file required")
+        };
+        let test = match File::open(&Path::new(filename)).read_to_end() {
+            Ok(val) => { 
+                match String::from_utf8(val) {
+                    Ok(val) => val,
+                    Err(err) => fail!(err)
+                } 
+            },
+            Err(err) => fail!(err.desc)
+        };
 
-		let mut mem = Memory::new();
+        let mut mem = Memory::new();
 
-		interpret(test, &mut mem);
-	}
+        interpret(test, &mut mem);
+    }
 }
